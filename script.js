375d266b6c965696d92fe673910511dfbf2d9740
mohamed <mojahdontheway@gmail.com>
2025-11-10 11:20:33 +0100

    Creer le bouton ajouter pour les section competence et langue ect...

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const sections = document.querySelectorAll('.section');
 
 const steps = document.querySelectorAll('.step')
@@ -35,7 +36,59 @@ backBtn.addEventListener('click', () => {
     sections[index].classList.add('active')
     steps[index].style.backgroundColor = "lightblue";
 
-    
+});
 
+// Add languages and competence
+
+const list = document.querySelectorAll('.lists');
+
+const yourInput = document.querySelectorAll('.your-input')
+
+const selectOption = document.querySelectorAll('.niveau');
+
+const addBtns = document.querySelectorAll('.add-btn');
+
+addBtns.forEach((btn, index) =>{
+    btn.addEventListener('click' , () =>{
+     
+    const input = yourInput[index].value;
+    const select = selectOption[index].value;
+    const div = document.createElement("div");
+    const p = document.createElement("p");
+    const span = document.createElement("span");
+    
+    div.classList.add("lang-item");
+
+    p.textContent = input;
+    span.textContent = select;
+
+    div.appendChild(p);
+    div.appendChild(span);
+    list[index].appendChild(div);    
 
 })
+
+});
+
+// add losires 
+
+const listOfloi = document.getElementById('list-loi');
+const inputOfloi = document.getElementById('input-loi');
+const addBtn = document.getElementById('add-btn');
+
+    addBtn.addEventListener('click' , () =>{
+    const value = inputOfloi.value;
+    const div = document.createElement("div");
+    const p = document.createElement("p");
+    
+    div.classList.add("lang-item");
+
+    p.textContent = value;
+
+    div.appendChild(p);
+    listOfloi.appendChild(div); 
+
+});
+
+
+
