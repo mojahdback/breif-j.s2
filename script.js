72519c51ccd1ac284ac44f97006a5dee153a81c1
mohamed <mojahdontheway@gmail.com>
2025-11-11 07:42:10 +0100

    creat un button pour supprimer les champs ajouter

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ backBtn.addEventListener('click', () => {
 
 });
 
-// Add languages and competence
+// Add button "Ajouter" for languages and competence
 
 const list = document.querySelectorAll('.lists');
 
@@ -56,21 +56,32 @@ addBtns.forEach((btn, index) =>{
     const div = document.createElement("div");
     const p = document.createElement("p");
     const span = document.createElement("span");
+    const delBtn = document.createElement('button')
     
     div.classList.add("lang-item");
 
     p.textContent = input;
     span.textContent = select;
+    delBtn.textContent = "Delete";
+
+    delBtn.style.marginLeft="10px";
+    delBtn.style.background="transparent";
 
     div.appendChild(p);
     div.appendChild(span);
-    list[index].appendChild(div);    
+    div.appendChild(delBtn);
+    list[index].appendChild(div);  
+    
+    delBtn.addEventListener('click', () => {
+
+        div.remove();
+    })
 
 })
 
 });
 
-// add losires 
+// add  boutton "Ajouter" for losires 
 
 const listOfloi = document.getElementById('list-loi');
 const inputOfloi = document.getElementById('input-loi');
@@ -80,15 +91,83 @@ const addBtn = document.getElementById('add-btn');
     const value = inputOfloi.value;
     const div = document.createElement("div");
     const p = document.createElement("p");
+    const delBtn = document.createElement('button')
     
     div.classList.add("lang-item");
 
     p.textContent = value;
+    delBtn.textContent = "Delete";
+
+    delBtn.style.marginLeft="10px";
+    delBtn.style.background="transparent";
+   
+    
 
     div.appendChild(p);
+    div.appendChild(delBtn);
     listOfloi.appendChild(div); 
 
+    delBtn.addEventListener('click', () => {
+
+        div.remove();
+    })
+
 });
+
+// add boutton "Ajouter" for  Expériences
+
+const listExp = document.getElementById('list-exp');
+const inputFon = document.getElementById('input-fonc');
+const inputLoc = document.getElementById('input-loca');
+const inputDes = document.getElementById('input-des');
+const howMuch = document.getElementById('how-much');
+const btnExp = document.getElementById('bouton');
+
+  btnExp.addEventListener('click' , () => {
+    let valueFon = inputFon.value;
+    let valueLoc = inputLoc.value;
+    let valueNum = howMuch.value;
+    // let valueDes = inputDes.value;
+
+    const div = document.createElement('div');
+    const p1 = document.createElement('p');
+    const p2 = document.createElement('p');
+    // const p3 = document.createElement('p');
+    const span = document.createElement('span');
+    const delBtn = document.createElement('button')
+
+    
+    div.classList.add("lang-item");
+     
+    p1.textContent= valueFon;
+    p2.textContent= valueLoc;
+    // p3.textContent= valueDes;
+    span.textContent= valueNum;
+
+    delBtn.textContent = "Delete";
+    delBtn.style.marginLeft="10px";
+    delBtn.style.background="transparent";
+
+    
+
+     div.appendChild(p1);
+     div.appendChild(p2);
+    //  div.appendChild(p3);
+     div.appendChild(span)
+    div.appendChild(delBtn);
+     listExp.appendChild(div); 
+
+      delBtn.addEventListener('click', () => {
+
+        div.remove();
+    })
+
+
+     
+
+})
+
+
 
 
 
